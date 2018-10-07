8fb7f6f32efe1538cb9aae1388b27bd5795506a6
hisomura <hisomura.noreply@gmail.com>
2018-10-07 12:42:45 +0900

    .envの位置を変更

--- utils.php
+++ utils.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_skyway_api_key() {
-    $f = new SplFileObject( __DIR__ . '/../.env');
+    $f = new SplFileObject( __DIR__ . '/.env');
     $f->setFlags(SplFileObject::READ_AHEAD | SplFileObject::SKIP_EMPTY | SplFileObject::DROP_NEW_LINE);
     $api_key = null;
     foreach ($f as $line) {
